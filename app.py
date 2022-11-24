08ac35bc504026c6be291f20a234ca258bfb3776
chrimson <chrimson@gmail.com>
2022-11-23 22:38:16 -0500

    Implement terminal text color, clear

--- app.py
+++ app.py
@@ -1,33 +1,42 @@
 from flask import Blueprint, render_template, request, session
 import paramiko
+import sys
 import time
 
 sht_bp = Blueprint('sht', __name__, static_folder='', template_folder='')
 
 ssh = paramiko.SSHClient()
 ssh.set_missing_host_key_policy(paramiko.AutoAddPolicy())
-ssh.connect('', username='', password='', port=22)
-channel = ssh.invoke_shell()
-
 
 @sht_bp.route('/', methods=['GET', 'POST'])
 def sht():
+  global channel
 
   if request.method == 'GET':
-    if 'log' not in session:
-      while not channel.recv_ready():
-        time.sleep(1)
-      out = channel.recv(8192).decode()
-      session['log'] = out
-    else:
-      out = session['log']
-    return render_template('base.html', app='sht', shell=out, content='templates/sht.html')
+    ssh.connect('127.0.0.1', username='ubuntu', password='12345', port=22)
+    channel = ssh.invoke_shell()
+    while not channel.recv_ready():
+      time.sleep(0.25)
+    out = channel.recv(8192).decode()
+    session['log'] = out
+    return render_template('base.html', app='sht',
+      shell=out, content='templates/sht.html')
 
   if request.method == 'POST':
     command = request.form.get('command')
     channel.send(command + '\n')
     while not channel.recv_ready():
-      time.sleep(1)
-    out = channel.recv(8192).decode()
-    session['log'] = session['log'] + out
+      time.sleep(0.25)
+    out_raw = channel.recv(8192)
+    out = out_raw.decode()
+    print(out_raw, file=sys.stdout)
+    if command == 'clear':
+      session['log'] = out[13:]
+    else:
+      out = out.\
+        replace('\x1b[31m', '<span style="color:red">').\
+        replace('\x1b[01;34m', '<span style="color:deepSkyBlue">').\
+        replace('\x1b[m', '</span>').\
+        replace('\x1b[0m', '</span>')
+      session['log'] = session['log'] + out
     return session['log']
